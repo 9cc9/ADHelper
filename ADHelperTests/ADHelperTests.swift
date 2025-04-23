c804575b0730a5f44671765cf37b464482211ddb
9cc9 <cyx199021@163.com>
2025-04-23 14:57:07 +0800

    Initial Commit

---
+++ ADHelperTests/ADHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADHelperTests.swift
+//  ADHelperTests
+//
+//  Created by 贝贝 on 2025/4/23.
+//
+
+import Testing
+@testable import ADHelper
+
+struct ADHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
